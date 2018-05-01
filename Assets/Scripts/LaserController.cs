b991eb6b54b07ec4eaf80275af0fc8081c93af74
Unknown <jopenmolles@hotmail.com>
2018-05-01 17:21:56 +0200

    BouncingLaser

---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour {
+
+    public float WaitForMoveTime = 5;
+    public int MovementSpeed = 5;
+
+    private bool _waitForMove = true;
+    private Rigidbody _rigidbody;
+	
+	// Update is called once per frame
+	void Update () {
+        if(_waitForMove)
+        {
+            WaitForMoveTime -= Time.deltaTime;
+            if (WaitForMoveTime <= 0)
+                _waitForMove = false;
+            return;
+        }    
+
+        if (_rigidbody.velocity.magnitude <= 0.1f)
+        {
+            _rigidbody.velocity = transform.forward * MovementSpeed;
+        }
+        else
+        {
+            _rigidbody.velocity = _rigidbody.velocity.normalized * MovementSpeed;
+        }
+    }
+
+    /// <summary>
+    /// Initialize the object
+    /// </summary>
+    private void OnEnable()
+    {
+        // Initialize fields
+        _rigidbody = GetComponent<Rigidbody>();
+        transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+    }
+}
